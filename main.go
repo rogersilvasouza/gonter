221aecbea9429be766fa595b3c087789c548fcb4
Roger Souza <rogersilvasouza@hotmail.com>
2023-04-09 06:27:01 -0300

    feat(gonter): Add go chromeDp and ignore vendor

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/chromedp/chromedp"
+)
 
 func main() {
 	fmt.Println("Gonter")
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	err := chromedp.Run(ctx, chromedp.Navigate("https://instagram.com/rogersilvasouza"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var spanText string
+	err = chromedp.Run(ctx, chromedp.Text("span", &spanText, chromedp.NodeVisible))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(spanText)
 }
