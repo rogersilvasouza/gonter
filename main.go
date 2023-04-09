fd3cc98d8cdf13f8fe81464ebeac69755a39a571
Roger Souza <rogersilvasouza@hotmail.com>
2023-04-09 07:45:10 -0300

    feat(gonter): Add viper and config.json

--- main.go
+++ main.go
@@ -3,8 +3,26 @@ Copyright © 2023 Roger Souza <rogersilvasouza@hotmail.com>
 */
 package main
 
-import "github.com/rogersilvasouza/gonter/internal/google"
+import (
+	"github.com/rogersilvasouza/gonter/internal/google"
+	"github.com/spf13/viper"
+)
 
 func main() {
+	viper.SetDefault("urls", []string{})
+	viper.SetConfigType("json")
+	viper.SetConfigFile("config.json")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	urls := viper.GetStringSlice("urls")
+
+	for _, url := range urls {
+		fmt.Println(url)
+	}
+
 	google.GetResult("g1.globo.com")
 }
